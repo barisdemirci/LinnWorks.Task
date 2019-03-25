e29ce61c9c3662e58a265198c52bec921dccf03d
baris-demirci <baris.demirci@accenture.com>
2019-03-25 11:35:49 +0200

    Bug fix

--- src/FrontEnd/LinnWorks.Task.FrontEnd/ClientApp/src/components/ImportFile.js
+++ src/FrontEnd/LinnWorks.Task.FrontEnd/ClientApp/src/components/ImportFile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { QueueApi } from '../api/QueueApi';
+import { QueueApi } from '../api/queueApi';
 
 export class ImportFile extends Component {
     componentWillMount() {
